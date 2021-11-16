9b2cf664884faa06ee73c7b32bbf96662533d773
Bruno Tetzner <brunoltetzner@gmail.com>
2021-11-16 15:03:11 -0300

    toDo funcionando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
-// import { Form } from "./styles";
-import SigUp from "./components/Signup";
+import React from "react";
+import { useState } from "react";
+import ToDo from "./components/todoList ";
+//criar o input e o botão que recebe o valor e guardar
+//Passar para como parametro apra um componente que ira criar esse elemento usando html
+
 function App() {
-  return <SigUp />;
+  const [toDoList, setToDoList] = useState([]);
+  const [inputValue, setInputValue] = useState();
+
+  const addToDo = (newToDo) => setToDoList([...toDoList, newToDo]);
+  const removeToDo = (itemToRemove) =>
+    setToDoList(toDoList.filter((item) => item !== itemToRemove));
+  return (
+    <>
+      <div>
+        <ToDo toDoList={toDoList} removeToDo={removeToDo} />
+        <input
+          onChange={(event) => setInputValue(event.target.value)}
+          placeholder="digite sua tarefa"
+          type="text"
+        ></input>
+        <button onClick={() => addToDo(inputValue)}>add</button>
+      </div>
+    </>
+  );
 }
 export default App;
